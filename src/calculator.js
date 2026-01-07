2206c6092920c7a2d075fc1c4cc8f806c6a92a51
Doug Zuniga <158117640+softdevninja@users.noreply.github.com>
2026-01-06 22:05:14 -0500

    Add comments and documentation to calculator.js
    Updating comment header

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,11 @@
 /**
+ * @fileoverview Testing out javascript files on Rhel VM by creating a Calulator class and calling it in index.js
+ * @author Doug Zuniga <doug.biz@outlook.com>
+ * @version 1.0.0
+ * @license N/A
+ */
+
+/**
  * Add function: Takes a & b as a parameters and adds them together prior to returning the results.
  * 
  * @param {*} a 
